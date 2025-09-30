2891974f77a5807cd13de5c02bc9bff2f04668be
Kaja <kasw0001@stud.kea.dk>
2025-09-30 14:02:49 +0200

    lille ting

--- js/produktliste.js
+++ js/produktliste.js
@@ -20,29 +20,29 @@ function showFiltered() {
 let allData;
 
 
-fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&category=${category}`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=30&category=${category}`)
 .then((response) => response.json())
 .then((data) => {
     allData = data;
     showProducts(allData);
 }); 
     
-    function showProducts(data) {
-        console.log(data);
-        let markup = "";
-        data.forEach(product => {
-            markup += `<div class="product ${product.discount && "onSale"} ${product.soldout && "soldOut"}">
+function showProducts(data) {
+    console.log(data);
+    let markup = "";
+    data.forEach(product => {
+        markup += `<div class="product ${product.discount && "onSale"} ${product.soldout && "soldOut"}">
          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="tøj">
          <p><strong>${product.productdisplayname}</strong></p>
          <p>${product.articletype} | ${product.brandname}</p>
           <p>DKK <span>${product.price},-</span></p>
         <div class=${product.discount ? "sale" : "hide"}>
          <p>Now DKK ${Math.round(product.price - product.price * product.discount / 100)},-</p>
-         <p>${product.discount}%</p>
+         <p class="rød">${product.discount}%</p>
         </div>
          <a href="produkt.html?id=${product.id}">Read More</a>
       </div>`
-        });
+    });
 
 productContainer.innerHTML = `<div class="grid_1-1-1-1">${markup}</div>`;
 }
